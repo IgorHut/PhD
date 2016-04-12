70fbc8e42c9fd587c83eca70ceb31e2bc27d6e4d
Igor Hut <igorhut@gmail.com>
2016-04-12 16:39:24 +0200

    First three columns selected out, useData$Case turned into factors, deleted columns that sum abs<0
    Some wrangling done

--- Initial_Data_Wrangling.R
+++ Initial_Data_Wrangling.R
@@ -12,3 +12,39 @@ initData # Posto je initData tbl ovo je sasvim ok
 
 useData<-initData[-(1:3)] #Uklanjam prve tri kolone jer mi nece trebati za inicijalnu analizu
 
+useData
+
+# Let's make factors out of chr markings
+
+useData$Case<-as.factor(useData$Case)
+
+# Check the outcome
+
+levels(useData$Case)
+
+# Putting all the CANCER* factor levels iside one CANCER wraper 
+ 
+levels(useData$Case)<-sub("CANCER.*", "CANCER", levels(useData$Case))
+
+# Check the situation
+
+levels(useData$Case)
+
+summary(useData$Case)
+
+# Zeroes are a huge problem in terms of calculating mean, sd etc. so let's get rid of them
+# Keeping only the columns with abs(colSum)>=0.2
+
+useData1<-select(useData,abs(colSums(useData[2:length(useData)]))>=0.2)
+
+# Putting the first column back
+
+useData1<-cbind(useData[1],useData1)
+
+# Making a table again
+
+useData1<-tbl_df(useData1)
+
+useData1
+
+
